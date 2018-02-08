e3bf4646f4ccb9ed216ababd6728327dcd8984ff
corydave <corydave@users.noreply.github.com>
2018-02-08 11:10:34 -0500

    Create SubClass.java

---
+++ Day02-2018-02-13/SuperClassDemo/SubClass.java
@@ -0,0 +1,14 @@
+public class SubClass extends SuperClass {
+
+    private int subClassVariable;
+    SubClass() {
+        subClassVariable = -20;
+    }
+    SubClass(int a) {
+        subClassVariable = a;
+    }
+
+    public void showValue() {
+        System.out.print("SubClass: " + subClassVariable);
+    }
+}
