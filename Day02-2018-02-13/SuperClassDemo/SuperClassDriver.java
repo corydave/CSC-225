8c7a8c61e4ef60474ed068720ace89ac834ce2c8
corydave <corydave@users.noreply.github.com>
2018-02-08 11:10:49 -0500

    Create SuperClassDriver.java

---
+++ Day02-2018-02-13/SuperClassDemo/SuperClassDriver.java
@@ -0,0 +1,7 @@
+public class SuperClassDriver {
+    public static void main (String[] args) {
+        SuperClass supClass = new SuperClass(10);
+        SubClass subClass = new SubClass(20);
+        SuperClass subClass2 = new SubClass(30);
+    }
+}
