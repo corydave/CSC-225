4b257313b740deb51eb09d5643ba029293badb27
corydave <corydave@users.noreply.github.com>
2018-03-01 09:18:18 -0500

    Create Battle.java

---
+++ LABS/DnDBattle/Battle.java
@@ -0,0 +1,41 @@
+public class Battle {
+
+  public Battle() {
+  }
+  
+  public Battle(Character playerOne, Character playerTwo) {
+  
+  /*
+   * This instantiation will bring both players into the arena
+   */
+   
+  }
+  
+  public Character[] superSmash() {
+   
+   * The battle will begin, and consists of phases:
+   *   1 - Collect the vital stats from each character and report to the other character
+   *   2 - One of the players will randomly be selected to go first
+   *   3 - Algorithmically, the specialEffect() method may be called on one or both Characters
+   *   4 - The first player will attack the second player
+   * . 5 - The battle is over if the health of the second player is 0 or below
+   *   6 - The second player will attack the first player
+   *   7 - The battle is over if the health of the first player is 0 or below
+   *  
+   *   The battle will repeat until a health check returns 0 or below.
+   * 
+   *   An array (of size 2) will be returned, index 0 contains the winner and 
+   *   index 1 contains the loser.
+   *
+   * The vital stats for each character will be collected, stored into an array, and sent to 
+   * the opposing Character. That is, there is a method called getOppInfo(Character)
+   * that will be called by each of the Characters in the Battle class.
+   *
+   * The array will be of type int and have the following structure:
+   *       [currentHealth, maxHealth, strength, defense, special, points]
+   *
+   * 
+   */
+  
+  
+  }
