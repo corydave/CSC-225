63218aed40353bfab9c50b31628842608f6af6fb
corydave <corydave@users.noreply.github.com>
2018-03-13 10:24:49 -0400

    Update JavaFXDemo.java
    Updated GUI components and functionality

--- DAY04-2018-03-13/JavaFXDemo.java
+++ DAY04-2018-03-13/JavaFXDemo.java
@@ -20,6 +20,7 @@ import javafx.stage.Stage;
 public class JavaFXDemo extends Application {
 
     public static void main(String[] args) {
+        System.out.println("TESTING JAVAFX");
         launch(args);
     }
 
@@ -29,7 +30,95 @@ public class JavaFXDemo extends Application {
         primaryStage.setTitle("JavaFX Welcome");
         GridPane grid = new GridPane();
 
-        Scene scene = new Scene(grid, 500, 575);
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        // Sweet, sweet text
+        Text scenetitle = new Text("Welcome!");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 30)); // Fashion
+        grid.add(scenetitle,0,0,2,1);
+
+
+        // Sweet, sweet label
+        Label userName = new Label("User Name:");
+        grid.add(userName,0,1);
+
+        // Sweet, sweet text field
+        TextField userTextField = new TextField();
+        grid.add(userTextField,1,1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw,0,2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox,1,2);
+
+        Image img = new Image("doogo.jpg");
+        ImageView imgView = new ImageView(img);
+        grid.add(imgView,0,3,2,1);
+
+        Button btn = new Button("Login");
+        HBox hbBtn = new HBox(10);
+        hbBtn.getChildren().add(btn);
+
+        Button btn2 = new Button("Tip of the day");
+        hbBtn.getChildren().add(btn2);
+
+        Button btn3 = new Button("Quit");
+        hbBtn.getChildren().add(btn3);
+
+        grid.add(hbBtn,0,4,2,1);
+
+        Label actiontarget = new Label();
+        grid.add(actiontarget,0,5,2,1);
+
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("'Login' PRESSED");
+                actiontarget.setText("Incorrect username or password.");
+            }
+
+        });
+
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("'Tip-O-The-Day' PRESSED");
+                actiontarget.setStyle("-fx-padding:10px; -fx-border-color:red; -fx-background-color:lightblue;");
+                actiontarget.setText("Make coffee cubes for iced coffee.");
+            }
+
+        });
+
+        btn3.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("'QUIT' PRESSED");
+                System.exit(0);
+            }
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Scene scene = new Scene(grid, 500, 775);
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -42,5 +131,7 @@ public class JavaFXDemo extends Application {
 
 
 
+
     }
 }
+
